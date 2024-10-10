d1804b4699f593c7ba99e29689990661d6287ccb
forever-faceless <poorvik1212@gmail.com>
2024-10-10 21:04:11 +0530

    script updated

---
+++ build/script.js
@@ -0,0 +1,15 @@
+
+
+const requestJoke = async () => {
+  const responseJoke = await fetch("https://icanhazdadjoke.com/", {
+    headers: { Accept: "application/json" },
+  });
+  const responseJOSN = await responseJoke.json();
+  const joke = responseJOSN.joke;
+  printToConsole(joke);
+};
+
+const printToConsole = (joke) => {
+  console.log(joke);
+};
+requestJoke();
